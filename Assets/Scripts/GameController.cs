e70b8955c81e9dfa4a1006799499f5419b773831
riddeshm <35489487+riddeshm@users.noreply.github.com>
2024-04-25 21:50:08 +0530

    Added scoring system
    - changed from hiding save button to disabling save button on game complete

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class GameController : MonoBehaviour
 {
-    public event System.Action OnGameComplete;
+    public event Action OnGameComplete;
+    public event Action<int, int> OnScoreUpdated;
 
     [SerializeField] private Board board;
 
@@ -15,6 +17,12 @@ public class GameController : MonoBehaviour
     public Card[] Cards { get { return board.Cards; } }
 
     public int matchCount = 0;
+
+    private int score = 0;
+    private int streak = 0;
+
+    private int correctPoints = 10;
+    private int incorrectPoints = 5;
 
     private void Start()
     {
@@ -40,6 +48,8 @@ public class GameController : MonoBehaviour
 
     private void Init()
     {
+        score = 0;
+        streak = 0;
         matchCount = 0;
         selectedCards.Clear();
     }
@@ -60,13 +70,21 @@ public class GameController : MonoBehaviour
             if(card1.GetId() == card2.GetId())
             {
                 matchCount++;
+                streak++;
+                score += (correctPoints * streak);
                 board.CorrectPair(card1, card2);
             }
             else
             {
+                if(score >= incorrectPoints)
+                {
+                    score -= incorrectPoints;
+                }
+                streak = 0;
                 board.InCorrectPair(card1, card2);
             }
         }
+        OnScoreUpdated?.Invoke(score, streak);
         CheckWin();
     }
 
--- Assets/Scripts/UIController.cs
+++ Assets/Scripts/UIController.cs
@@ -1,23 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using System.Text;
 
 public class UIController : MonoBehaviour
 {
-    [SerializeField] GameController gameController;
+    [SerializeField] private GameController gameController;
     [SerializeField] private Canvas menuCanvas;
-    [SerializeField] SaveManager saveManager;
-    [SerializeField] GameObject saveButton;
-    [SerializeField] GameObject winPopup;
+    [SerializeField] private SaveManager saveManager;
+    [SerializeField] private Button saveButton;
+    [SerializeField] private GameObject winPopup;
+    [SerializeField] private TextMeshProUGUI scoreText;
+
+    private StringBuilder builder = new StringBuilder(50);
 
     private void Awake()
     {
         gameController.OnGameComplete += GameComplete;
+        gameController.OnScoreUpdated += UpdateScore;
     }
 
     private void OnDestroy()
     {
         gameController.OnGameComplete -= GameComplete;
+        gameController.OnScoreUpdated -= UpdateScore;
     }
 
     public void InitBoard2x2()
@@ -69,13 +77,24 @@ public class UIController : MonoBehaviour
     {
         menuCanvas.enabled = false;
         menuCanvas.gameObject.SetActive(false);
-        saveButton.SetActive(true);
+        saveButton.interactable = true;
         winPopup.SetActive(false);
+        scoreText.text = "Score : 0 Streak : 0";
     }
 
     private void GameComplete()
     {
-        saveButton.SetActive(false);
+        saveButton.interactable = false;
         winPopup.SetActive(true);
+    }
+
+    private void UpdateScore(int score, int streak)
+    {
+        builder.Length = 0;
+        builder.Append("Score : ");
+        builder.Append(score);
+        builder.Append(" Streak : ");
+        builder.Append(streak);
+        scoreText.text = builder.ToString();
     }
 }
